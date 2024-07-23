73a3efd616069a45896ae5438dc0f14e0a9ebd01
Youngdo, Lee <ydlee@innodep.com>
2024-07-23 12:55:24 +0900

    Remove verbose message from deplay.sh
    Signed-off-by: Youngdo, Lee <ydlee@innodep.com>

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,7 @@ hugo -t ananke
 # Go to public folder, submodule commit
 cd public
 # Add changes to git.
-echo "*** =============="
 git add .
-echo "=============="
 
 # Commit changes.
 msg="rebuilding site `date`"
@@ -19,20 +17,15 @@ if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-echo "=============="
 
 # Push source and build repos.
-echo "+++ =============="
 git push origin gh-pages
-echo "--- =============="
 
 # Come back up to the project root
 cd ..
 
 # Commit and push to master branch
 git add .
-
-echo "=============="
 
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
